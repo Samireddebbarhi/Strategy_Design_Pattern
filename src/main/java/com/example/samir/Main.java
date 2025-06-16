d1475274966eb1e034c7124dbc1549eef13237fc
Your Name <samireddebbarhi@gmail.com>
2025-06-16 10:08:51 +0100

    adding the entry point

--- src/main/java/com/example/samir/Main.java
+++ src/main/java/com/example/samir/Main.java
@@ -1,17 +1,68 @@
 package com.example.samir;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            double amount = 0;
+            System.out.print("Enter payment amount: $");
+            if (scanner.hasNextDouble()) {
+                amount = scanner.nextDouble();
+            }
+            scanner.nextLine();
+
+            System.out.println("Select payment method:");
+            System.out.println("1. PayPal");
+            System.out.println("2. GPay");
+            System.out.println("3. Credit Card");
+            System.out.print("Your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            PaymentStrategy strategy = null;
+
+            // We can use Factory Design Pattern for creating strategies
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter your PayPal email: ");
+                    String email = scanner.nextLine();
+                    strategy = new PaypalPayment(email);
+                    break;
+                case 2:
+                    System.out.print("Enter your pin: ");
+                    String pin = scanner.nextLine();
+                    strategy = new GooglePayPayment(pin);
+                    break;
+                case 3:
+                    System.out.print("Enter your name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter your card number: ");
+                    String card = scanner.nextLine();
+                    System.out.print("Enter your CVV: ");
+                    String cvv = scanner.nextLine();
+                    System.out.print("Enter your expiry date (MM/YY): ");
+                    String expiry = scanner.nextLine();
+                    System.out.print("Enter your account type: ");
+                    String cardType = scanner.nextLine();
+                    System.out.print("Enter your Card Number: ");
+                    String cardNumber = scanner.nextLine();
+
+
+                    strategy = new CreditcardPayment(name, card, cvv, expiry,cardType);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Exiting.");
+                    return;
+            }
+            // Use context to process payment
+            PaymentProcessor processor = new PaymentProcessor(strategy);
+            processor.processPayment(amount);
+
+            scanner.close();
         }
     }
-}
